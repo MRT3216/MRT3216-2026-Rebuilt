8f181fec7fe2e107b4407a77df4380b389a675f1
William <148827581+DaNoobMan@users.noreply.github.com>
2026-03-13 13:44:16 -0700

    Adjusted shooting angles and used autonomousPeriodic to loop autos
    While looping is not necessary for the long autos that are being used currently, if I make a super efficient short auto in the future, it can loop for the entirety of the autonomous period.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -198,7 +198,10 @@ public class Robot extends LoggedRobot {
 
     /** This function is called periodically during autonomous. */
     @Override
-    public void autonomousPeriodic() {}
+    public void autonomousPeriodic() {
+        CommandScheduler.getInstance().run();
+        CommandScheduler.getInstance().schedule(autonomousCommand);
+    }
 
     /** This function is called once when teleop is enabled. */
     @Override

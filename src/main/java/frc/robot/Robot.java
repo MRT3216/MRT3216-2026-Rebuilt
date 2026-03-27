c5d0167506e3691a3cb38fab43a79c0140090d1a
William <148827581+DaNoobMan@users.noreply.github.com>
2026-03-27 11:09:29 -0600

    Reschedule autos

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -240,7 +240,9 @@ public class Robot extends LoggedRobot {
 
     /** This function is called periodically during autonomous. */
     @Override
-    public void autonomousPeriodic() {}
+    public void autonomousPeriodic() {
+        CommandScheduler.getInstance().schedule(autonomousCommand);
+    }
 
     /** This function is called once when teleop is enabled. */
     @Override
